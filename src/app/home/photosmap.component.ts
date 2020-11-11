284bae4928be52b539dc6ab841da9a9737c59738
Daniel Faderski <daniel.faderski@allegro.pl>
2020-11-11 12:00:20 +0100

    Taking photos at location

--- src/app/home/photosmap.component.ts
+++ src/app/home/photosmap.component.ts
@@ -5,7 +5,10 @@ import {RadSideDrawerComponent} from "nativescript-ui-sidedrawer/angular";
 import {RadSideDrawer} from "nativescript-ui-sidedrawer";
 import {TNSFontIconService} from "nativescript-ngx-fonticon";
 import * as geolocation from "@nativescript/geolocation";
+import * as camera from "@nativescript/camera";
 import {Accuracy} from "@nativescript/core/ui/enums";
+import {Image} from "@nativescript/core/ui";
+
 
 @Component({
     templateUrl: "./photosmap.component.html",
@@ -16,6 +19,8 @@ export class PhotosMapComponent implements AfterViewInit {
     routerExtensions: RouterExtensions
     GEOLOCATION_TIMEOUT = 20000
     GEOLOCATION_MAX_AGE = 5000
+    PHOTO_WIDTH = 200
+    PHOTO_HEIGHT = 200
 
     constructor(client: ServerClient, routerExtensions: RouterExtensions,
                 fontIconService: TNSFontIconService) {
@@ -26,8 +31,10 @@ export class PhotosMapComponent implements AfterViewInit {
     @ViewChild(RadSideDrawerComponent, {static: false}) public drawerComponent: RadSideDrawerComponent;
     private drawer: RadSideDrawer;
 
-    ngAfterViewInit() {
+    async ngAfterViewInit() {
         this.drawer = this.drawerComponent.sideDrawer;
+        await geolocation.enableLocationRequest();
+        await camera.requestPermissions();
     }
 
     toggleDrawer() {
@@ -39,9 +46,22 @@ export class PhotosMapComponent implements AfterViewInit {
     }
 
     async takePhoto() {
-        await geolocation.enableLocationRequest();
-        let location = await geolocation.getCurrentLocation(
-            {desiredAccuracy: Accuracy.high, maximumAge: this.GEOLOCATION_MAX_AGE, timeout: this.GEOLOCATION_TIMEOUT});
-
+        try {
+            let location = await geolocation.getCurrentLocation(
+                {
+                    desiredAccuracy: Accuracy.high,
+                    maximumAge: this.GEOLOCATION_MAX_AGE,
+                    timeout: this.GEOLOCATION_TIMEOUT
+                });
+            let asset = await camera.takePicture(
+                {saveToGallery: true, width: this.PHOTO_WIDTH, height: this.PHOTO_HEIGHT, keepAspectRatio: true});
+            let image = new Image();
+            console.log(asset)
+            // image.src = asset;
+            // send image to server
+        } catch (err) {
+            console.dir(err);
+            alert("Sorry, something gone wrong :( Try again")
+        }
     }
 }
