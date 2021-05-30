7ad684fce8e591c518436e25ec626193b0e85405
Daniel Faderski <daniel.faderski@allegro.pl>
2021-05-30 20:38:59 +0200

    Gallery swipe animation

--- src/app/gallery/gallery.ts
+++ src/app/gallery/gallery.ts
@@ -36,13 +36,23 @@ export class Gallery {
     }
 
     takeNextPhoto(photo: GalleryPhotoAtLocation): GalleryPhotoAtLocation {
-        if (this.photos.length== 0) {
+        if (this.photos.length == 0) {
             return null;
         }
         if (this.photos.length > photo.index + 1) {
             return this.photos[photo.index + 1];
         }
         return this.photos[this.photos.length - 1];
+    }
+
+    takePrevPhoto(photo: GalleryPhotoAtLocation): GalleryPhotoAtLocation {
+        if (this.photos.length == 0) {
+            return null;
+        }
+        if (photo.index - 1 >= 0) {
+            return this.photos[photo.index - 1];
+        }
+        return this.photos[0];
     }
 
     static empty() {
